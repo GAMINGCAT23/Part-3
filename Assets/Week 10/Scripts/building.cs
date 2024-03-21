fb020085bf96e538cfb7e26f1afcd3da645ffa4e
GAMINGCAT23 <148483729+GAMINGCAT23@users.noreply.github.com>
2024-03-21 12:24:07 -0400

    Task3 finish

--- Assets/Week 10/Scripts/building.cs
+++ Assets/Week 10/Scripts/building.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class building : MonoBehaviour
 {
     public GameObject[] buildingParts;
-    public float totalAnimationDuration = 1.5f;
-    public float gapBetweenPhases = 0.1f;
+    public float Speed = 1.5f;
+    public float Gap = 0.1f;
 
     void Start()
     {
@@ -17,26 +17,19 @@ public class building : MonoBehaviour
     {
         foreach (GameObject part in buildingParts)
         {
-            yield return StartCoroutine(ZoomGameObject(part.transform));
-            yield return new WaitForSeconds(gapBetweenPhases);
+            float TargetTime = 0f;
+            Vector3 NowScale = Vector3.zero;
+            Vector3 targetScale = part.transform.localScale;
+
+            while (TargetTime < Speed)
+            {
+                part.transform.localScale = Vector3.Lerp(NowScale, targetScale, (TargetTime / Speed));
+                TargetTime += Time.deltaTime;
+                yield return null;
+            }
+
+            part.transform.localScale = targetScale;
+            yield return new WaitForSeconds(Gap);
         }
-    }
-
-    IEnumerator ZoomGameObject(Transform objTransform)
-    {
-        Vector3 initialScale = Vector3.zero;
-        Vector3 targetScale = objTransform.localScale;
-
-        float phaseDuration = totalAnimationDuration / 3f;
-        float elapsedTime = 0f;
-
-        while (elapsedTime < phaseDuration)
-        {
-            objTransform.localScale = Vector3.Lerp(initialScale, targetScale, (elapsedTime / phaseDuration));
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        objTransform.localScale = targetScale;
     }
 }
